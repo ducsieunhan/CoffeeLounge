62874df8065c82a31eba750d165b9e468921cf8c
ducsieunhan <Chroller03@gmail.com>
2024-12-24 21:25:23 +0700

    start the web

---
+++ src/main/java/com/example/CoffeeLounge/CoffeeLoungeApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoffeeLounge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeLoungeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeLoungeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CoffeeLounge/CoffeeLoungeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CoffeeLounge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeLoungeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
